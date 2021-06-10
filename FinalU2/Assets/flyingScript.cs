619bb193806b5a864356c8277a5d5765cadbde1b
Sebalaur <75026257+Sebalaur@users.noreply.github.com>
2021-06-10 18:09:03 +0300

    recuperare

---
+++ FinalU2/Assets/flyingScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flyingScript : MonoBehaviour
+{
+    public float baseSpeed;
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = baseSpeed + GvrVRHelpers.GetHeadRotation().x *10;
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            transform.position + GvrVRHelpers.GetHeadForward(),
+            speed * Time.deltaTime
+        );
+    }
+}
